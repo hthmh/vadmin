045bddde64ab039698b2b8f473dfc04883b1067f
pj <pj_panjian@163.com>
2017-11-16 11:24:36 +0800

    add

---
+++ static/js/16.ed7907ea7c4dd1c3adfd.js
@@ -0,0 +1,1 @@
+webpackJsonp([16],{aaMa:function(e,t,a){"use strict";var n=function(){var e=this,t=e.$createElement,a=e._self._c||t;return a("div",[a("router-view")],1)},r=[],u={render:n,staticRenderFns:r};t.a=u},iuFH:function(e,t,a){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n=a("aaMa"),r=a("/Xao"),u=r(null,n.a,!1,null,null,null);t.default=u.exports}});
